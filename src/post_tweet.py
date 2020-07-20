85425c3476fab994355200155c64d113883ad232
Colin Spear <colinspear@gmail.com>
2020-07-20 16:49:16 -0700

    add notes for scheduling post

--- src/post_tweet.py
+++ src/post_tweet.py
@@ -4,8 +4,8 @@ from pathlib import Path
 import pandas as pd
 
 
-home = Path().cwd()
-df = pd.read_csv(home / 'data/clean_rejections.csv')
+home = '/Users/colinspear/dev/vanity_plates/'
+df = pd.read_csv(home + 'data/clean_rejections.csv')
 
 tweet = t.make_tweet(df)
 t.post_tweet(tweet)
